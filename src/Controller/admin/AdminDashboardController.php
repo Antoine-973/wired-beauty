8db0e4b5961578b20f06187e3a385127b109a8dc
Antoine Saunier <antoine.saunier.pro@gmail.com>
2022-03-08 01:00:47 +0100

    [WIP] Front office / Back office route architecture

---
+++ src/Controller/admin/AdminDashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminDashboardController extends AbstractController
+{
+    #[Route('/', name: 'app_admin_dashboard')]
+    public function index(): Response
+    {
+        return $this->render('admin_dashboard/index.html.twig', [
+            'controller_name' => 'AdminDashboardController',
+        ]);
+    }
+}

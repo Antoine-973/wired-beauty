1607c7e4e2ff09fd413bc4b688f3c8236c3c3334
Antoine Saunier <antoine.saunier.pro@gmail.com>
2022-03-08 13:45:57 +0100

    fix no route for /admin bug

--- src/Controller/admin/AdminDashboardController.php
+++ src/Controller/admin/AdminDashboardController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin')]
 class AdminDashboardController extends AbstractController
 {
     #[Route('/', name: 'app_admin_dashboard')]

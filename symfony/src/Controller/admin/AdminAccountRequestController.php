61844ced204f26e62dece0e3dcfde0c81233ef3c
Antoine Saunier <antoine.saunier.pro@gmail.com>
2022-03-10 15:42:06 +0100

    functionnal reset email password after validating account request

--- symfony/src/Controller/admin/AdminAccountRequestController.php
+++ symfony/src/Controller/admin/AdminAccountRequestController.php
@@ -8,17 +8,32 @@ use App\Form\AccountRequest1Type;
 use App\Repository\AccountRequestRepository;
 use App\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\ResetPassword\Controller\ResetPasswordControllerTrait;
+use SymfonyCasts\Bundle\ResetPassword\Exception\ResetPasswordExceptionInterface;
+use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
 
 #[Route('/admin/user/request')]
 class AdminAccountRequestController extends AbstractController
 {
+    use ResetPasswordControllerTrait;
+
+    private $resetPasswordHelper;
+
+    public function __construct(ResetPasswordHelperInterface $resetPasswordHelper)
+    {
+        $this->resetPasswordHelper = $resetPasswordHelper;
+    }
+
     #[Route('/', name: 'app_admin_account_request_index', methods: ['GET'])]
     public function index(AccountRequestRepository $accountRequestRepository): Response
     {
@@ -36,7 +51,7 @@ class AdminAccountRequestController extends AbstractController
     }
 
     #[Route('/{id}/validate', name: 'app_admin_account_request_validate', methods: ['GET', 'POST'])]
-    public function validate(Request $request, AccountRequest $accountRequest, AccountRequestRepository $accountRequestRepository, UserPasswordHasherInterface $userPasswordHasherInterface, UserRepository $userRepository, MailerInterface $mailer): Response
+    public function validate(Request $request, AccountRequest $accountRequest, AccountRequestRepository $accountRequestRepository, UserPasswordHasherInterface $userPasswordHasherInterface, UserRepository $userRepository, MailerInterface $mailer, TranslatorInterface $translator): Response
     {
         if ($this->isCsrfTokenValid('validate'.$accountRequest->getId(), $request->request->get('_token'))) {
             $accountRequest->setIsValid(true);
@@ -55,6 +70,33 @@ class AdminAccountRequestController extends AbstractController
             $user->setPassword($userPasswordHasherInterface->hashPassword($user, $password));
 
             $userRepository->add($user);
+
+            try {
+                $resetToken = $this->resetPasswordHelper->generateResetToken($user);
+            } catch (ResetPasswordExceptionInterface $e) {
+                // If you want to tell the user why a reset email was not sent, uncomment
+                // the lines below and change the redirect to 'app_forgot_password_request'.
+                // Caution: This may reveal if a user is registered or not.
+                //
+                $this->addFlash('reset_password_error', sprintf(
+                    '%s - %s',
+                    $translator->trans(ResetPasswordExceptionInterface::MESSAGE_PROBLEM_HANDLE, [], 'ResetPasswordBundle'),
+                    $translator->trans($e->getReason(), [], 'ResetPasswordBundle')
+                ));
+
+                return $this->redirectToRoute('app_admin_account_request_validate');
+            }
+
+            $email = (new TemplatedEmail())
+                ->from(new Address('wired-beauty@gmail.com', 'Wired Beauty'))
+                ->to($user->getEmail())
+                ->subject('Wired-Beauty - Your account request has been validated')
+                ->htmlTemplate('reset_password/email_validate_account_request.html.twig')
+                ->context([
+                    'resetToken' => $resetToken,
+                ]);
+
+            $mailer->send($email);
         }
 
         return $this->redirectToRoute('app_admin_account_request_index', [], Response::HTTP_SEE_OTHER);

92595cd0ba78ad6e12db6eea6181ceae32f629f9
JS <julian.saleix@gmail.com>
2022-03-11 16:44:27 +0100

    Manage Buy Report Request

--- symfony/src/Controller/admin/BuyReportRequestController.php
+++ symfony/src/Controller/admin/BuyReportRequestController.php
@@ -5,6 +5,7 @@ namespace App\Controller\admin;
 use App\Entity\BuyReportRequest;
 use App\Form\BuyReportRequestType;
 use App\Repository\BuyReportRequestRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,63 +15,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class BuyReportRequestController extends AbstractController
 {
     #[Route('/', name: 'app_admin_buy_report_request_index', methods: ['GET'])]
-    public function index(BuyReportRequestRepository $buyReportRequestRepository): Response
+    public function index(BuyReportRequestRepository $buyReportRequestRepository, Request $request): Response
     {
-        return $this->render('buy_report_request/index.html.twig', [
-            'buy_report_requests' => $buyReportRequestRepository->findAll(),
-        ]);
-    }
+        $param = $request->query->get('accepted');
 
-    #[Route('/new', name: 'app_admin_buy_report_request_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, BuyReportRequestRepository $buyReportRequestRepository): Response
-    {
-        $buyReportRequest = new BuyReportRequest();
-        $form = $this->createForm(BuyReportRequestType::class, $buyReportRequest);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $buyReportRequestRepository->add($buyReportRequest);
-            return $this->redirectToRoute('app_buy_report_request_index', [], Response::HTTP_SEE_OTHER);
+        if( in_array($param, ['accepted', 'false']) ){
+            $results = $buyReportRequestRepository->findBy(['accepted' => $param]);
+        }else{
+            $results = $buyReportRequestRepository->findAll();
         }
 
-        return $this->renderForm('buy_report_request/new.html.twig', [
-            'buy_report_request' => $buyReportRequest,
-            'form' => $form,
+        return $this->render('admin/buy_report_request/index.html.twig', [
+            'buy_report_requests' => $results,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_buy_report_request_show', methods: ['GET'])]
-    public function show(BuyReportRequest $buyReportRequest): Response
+    #[Route('/accept/{id}', name: 'app_admin_buy_report_request_accept', methods: ['GET'])]
+    public function accept(BuyReportRequest $buyReportRequest, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('buy_report_request/show.html.twig', [
-            'buy_report_request' => $buyReportRequest,
-        ]);
+        $buyReportRequest->setAccepted(true);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_admin_buy_report_request_index', [], Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('/{id}/edit', name: 'app_admin_buy_report_request_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, BuyReportRequest $buyReportRequest, BuyReportRequestRepository $buyReportRequestRepository): Response
+    #[Route('/deny/{id}', name: 'app_admin_buy_report_request_deny', methods: ['GET'])]
+    public function deny( BuyReportRequest $buyReportRequest, BuyReportRequestRepository $buyReportRequestRepository): Response
     {
-        $form = $this->createForm(BuyReportRequestType::class, $buyReportRequest);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $buyReportRequestRepository->add($buyReportRequest);
-            return $this->redirectToRoute('app_buy_report_request_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('buy_report_request/edit.html.twig', [
-            'buy_report_request' => $buyReportRequest,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_buy_report_request_delete', methods: ['POST'])]
-    public function delete(Request $request, BuyReportRequest $buyReportRequest, BuyReportRequestRepository $buyReportRequestRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$buyReportRequest->getId(), $request->request->get('_token'))) {
-            $buyReportRequestRepository->remove($buyReportRequest);
-        }
-
-        return $this->redirectToRoute('app_buy_report_request_index', [], Response::HTTP_SEE_OTHER);
+        $buyReportRequestRepository->remove($buyReportRequest);
+        return $this->redirectToRoute('app_admin_buy_report_request_index', [], Response::HTTP_SEE_OTHER);
     }
 }

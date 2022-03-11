dac1a25f0badd76139762b3db5db4a3c6492116d
Antoine Saunier <antoine.saunier.pro@gmail.com>
2022-03-11 16:50:40 +0100

    [WIP] push work for adel

---
+++ symfony/src/Controller/OrderController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ReportRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[IsGranted('ROLE_CLIENT')]
+#[Route('/{_locale<%app.supported_locales%>}/order')]
+class OrderController extends AbstractController
+{
+
+    #[Route('/', name: 'app_order')]
+    public function index(ReportRepository $reportRepository): Response
+    {
+        return $this->render('order/index.html.twig', [
+            'reports' => $reportRepository->findAll(),
+        ]);
+    }
+}
